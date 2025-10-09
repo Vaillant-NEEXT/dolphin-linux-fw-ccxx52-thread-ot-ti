4d53d3f4325c8ffe5621b6c9511ec057e0082daa
vmeunier <vincent.meunier@vaillant-group.com>
2025-10-09 09:59:48 +0200

    feat: enable backdoor pin and set gpio
    If missing CCU won't be able to reflash or even
    reboot the chip as default pin is not the same
    and backdoor disable
    
    []

--- src/syscfg/ti_devices_config.c
+++ src/syscfg/ti_devices_config.c
@@ -51,9 +51,14 @@
 // Enable ROM boot loader
 #define SET_CCFG_BL_CONFIG_BOOTLOADER_ENABLE            0xC5
 
-// Disabled boot loader backdoor
+// Enable boot loader backdoor
 #define SET_CCFG_BL_CONFIG_BL_ENABLE                    0xC5
 
+// DIO number for boot loader backdoor
+#define SET_CCFG_BL_CONFIG_BL_PIN_NUMBER                0x0E
+
+// Active low to open boot loader backdoor
+#define SET_CCFG_BL_CONFIG_BL_LEVEL                     0x0
 
 // Default address in IMAGE_VALID_CONF register
 #define SET_CCFG_IMAGE_VALID_CONF_IMAGE_VALID           0x00000000
